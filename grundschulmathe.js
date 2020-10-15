718463904398446ad8ee26b53519ff8fa6c16d4c
Philipp Kollmar <ph-kollmar@web.de>
2020-10-15 16:52:40 +0200

    changed if

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,10 +1,11 @@
 alert('Grundschulmathe')
 var number1 = Math.floor(Math.random()*20);
 var number2 = Math.floor(Math.random()*20);
+var solution = number1 + number2;
 
-var userNumber = prompt("Wieviel ist" + number1 + "+" + number2);
+var userNumber = prompt("Wieviel ist " + number1 + " + " + number2);
 
-if (userNumber == 23) {
+if (userNumber == solution) {
     alert('Korrekt')
 }
 else{
