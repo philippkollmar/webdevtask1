a520130131a87a53e03b4cad40528ed8c416664e
Philipp Kollmar <ph-kollmar@web.de>
2020-10-15 17:02:20 +0200

    added for loop and pointcounter

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,4 +1,10 @@
 alert('Grundschulmathe')
+
+var i;
+var points= 0;
+
+for (i = 0; i < 5; i++) {
+
 var number1 = Math.floor(Math.random()*20);
 var number2 = Math.floor(Math.random()*20);
 var solution = number1 + number2;
@@ -6,8 +12,9 @@ var solution = number1 + number2;
 var userNumber = prompt("Wieviel ist " + number1 + " + " + number2);
 
 if (userNumber == solution) {
-    alert('Korrekt')
+    alert('Korrekt '+ points + 1 +"/5")
 }
 else{
-    alert('falsche Antwort')
+    alert('falsche Antwort' + points + "/5")
+}
 }
